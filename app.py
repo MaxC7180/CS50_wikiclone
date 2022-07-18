34369332401cd4c02f814a1de26864449613d3ce
MaxC7180 <97999816+MaxC7180@users.noreply.github.com>
2022-07-18 00:07:37 +0000

    db

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def create_account():
             return render_template("create.html", taken = "Username is already in use")
 
         db.execute("INSERT INTO users VALUES (?, ?, ?, f, ?)", user_ID, user, password, email)
-        return render_template("create.html", login = login)
+        return render_template("create.html")
     return render_template("create.html")
 
 @app.route("/about", methods=["GET", "POST"])
